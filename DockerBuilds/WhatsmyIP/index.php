a57e1fc4ca195512b6602811079f817257d1c056
Jtaylorthegreat <46611199+Jtaylorthegreat@users.noreply.github.com>
2021-01-21 15:20:45 -0600

    Create index.php

---
+++ DockerBuilds/WhatsmyIP/index.php
@@ -0,0 +1,8 @@
+<?php
+echo "Backend Server IP:   ";
+echo $_SERVER["SERVER_ADDR"];
+echo "<br>------------------------<br>";
+echo "Client IP:    ";
+echo $_SERVER["REMOTE_ADDR"];
+echo "<br>------------------------";
+ ?>
