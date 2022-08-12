3b45de4ef712d685dbbcd667f70b276e418c75b3
Jtaylorthegreat <46611199+Jtaylorthegreat@users.noreply.github.com>
2022-08-12 12:19:59 -0500

    Delete DockerBuilds/WhatsmyIP directory

--- DockerBuilds/WhatsmyIP/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "Backend Server IP:   ";
-echo $_SERVER["SERVER_ADDR"];
-echo "<br>------------------------<br>";
-echo "Client IP:    ";
-echo $_SERVER["REMOTE_ADDR"];
-echo "<br>------------------------";
- ?>
